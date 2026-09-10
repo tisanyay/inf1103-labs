a523e35187115ece9957ddeafcc6ad0636995805
TAY BRENDEN LOMIBAO TAY YI SAN <2601795@sit.singaporetech.edu.sg>
2026-09-10 15:31:17 +0800

    added trigger for overstock alert

--- auditor.py
+++ auditor.py
@@ -1,6 +1,8 @@
 print("SMART AUDITOR PROGRAM!!!!")
 
-inventory = []
+STOCK_LIMIT = 500
+failed_entry_count = 0
+inventory = 0
 
 def is_positive(num):
     return int(num) > 0
@@ -8,17 +10,25 @@ def is_positive(num):
 def is_digit(num):
     return num.isdigit()
 
+def is_over_stock_limit(num):
+    return int(num) > STOCK_LIMIT
+
 def iterate_checks(list_of_checks, num):
     for check in list_of_checks:
         if not check(num):
             return False
     return True
 
+def print_status(units_processed, failed_entry_count):
+    print("Total units processed:", units_processed)
+    print("Number of failed/rejected entries:", failed_entry_count)
+
 while True:
     command = input("Enter stock quantity or enter 'quit': \n")
 
     if command == "quit":
         break
+
     
     list_of_checks = [
             is_digit,
@@ -27,8 +37,16 @@ while True:
 
     if not iterate_checks(list_of_checks, command):
         print("ERROR!!! CHECK YOUR STOCK QUANTITY AGAIN!!")
+        failed_entry_count += 1
         continue
 
-    inventory.append(command)
-
+    if is_over_stock_limit(inventory+int(command)):
+        print("ALERT: ENTRY EXCEEDS STOCK INVENTORY\nEXITING...")
+        break
+        
+    inventory += int(command)
     print(inventory)
+
+print(inventory)
+print_status(inventory, failed_entry_count)
+    
